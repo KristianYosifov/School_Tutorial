bab02bc1723e71f1d422878a63f6b9b9a68afc27
yodatime <electriclikethor@gmail.com>
2025-02-14 11:18:01 +0200

    Add project files.

---
+++ School_Tutorial/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace School_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
