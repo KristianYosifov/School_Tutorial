b659eea22cc2c8170afc8821d49220afd1fb2f8d
MEGA <MEGA@gmail.com>
2025-02-17 12:18:53 +0200

    add n

--- School_Tutorial/Program.cs
+++ School_Tutorial/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace School_Tutorial
+﻿using System.Runtime.CompilerServices;
+
+namespace School_Tutorial
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-
+            int n = 1;
+            Print(n);
+        }
+        static void Print(int n)
+        {
+            Console.WriteLine($"{n++} {Console.ReadLine()}");
         }
     }
 }
